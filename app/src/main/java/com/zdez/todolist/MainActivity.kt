30a367bc9a7bfc6d0e0897aab11798abf2649e1c
zdezak <zdezarus@gmail.com>
2021-07-03 13:42:38 +0500

    init

---
+++ app/src/main/java/com/zdez/todolist/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.zdez.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zdez.todolist.main.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, HomeFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
